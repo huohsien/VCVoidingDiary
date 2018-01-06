777b448018696b9eea482dbc36323c5f98e06f58
Victor Jiang <huohsien@me.com>
2018-01-07 01:59:17 +0800

    initial commit

---
+++ VCVoidingDiary/VCDayNightPickerViewController.swift
@@ -0,0 +1,41 @@
+//
+//  VCDayNightPickerViewController.swift
+//  VCVoidingDiary
+//
+//  Created by victor on 07/01/2018.
+//  Copyright © 2018 VHHC Studio. All rights reserved.
+//
+
+import UIKit
+
+class VCDayNightPickerViewController: UIViewController {
+
+    @IBOutlet weak var dayButton: UIButton!
+    @IBOutlet weak var nightButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+//        dayButton.titleLabel?.font = UIFont(name: "cwTeXKai", size: 100.0);
+//        nightButton.titleLabel?.font = UIFont(name: "cwTeXKai", size: 100.0);
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
